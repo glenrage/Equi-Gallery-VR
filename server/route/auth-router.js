f9c602d8fdba4e4100f809a391e27d67cd127c35
galang pham <phamgalang@gmail.com>
2017-06-14 16:35:02 -0700

    user routes work

---
+++ server/route/auth-router.js
@@ -0,0 +1,49 @@
+'use strict';
+
+const Router = require('express').Router;
+const createError = require('http-errors');
+const jsonParser = require('body-parser').json();
+const debug = require('debug')('equi-gallery:auth-router');
+const basicAuth = require('../lib/basic-auth-middleware.js');
+const bearerAuth = require('../lib/bearer-auth-middleware.js');
+const User = require('../model/user-model.js');
+
+const authRouter = module.exports = Router();
+
+authRouter.post('/api/signup', jsonParser, function(req, res, next) {
+  debug('POST api/signup');
+
+  let password = req.body.password;
+  delete req.body.password;
+  let user = new User(req.body);
+
+  if(!password)
+    return next(createError(400, 'password required'));
+  if(password.length < 6)
+    return next(createError(400, 'password must be 6 characters'));
+
+  user.generatePasswordHash(password)
+    .then(user => user.save())
+    .then(user => user.generateToken())
+    .then(token => res.send(token))
+    .catch(next);
+});
+
+authRouter.get('/api/login', basicAuth, function(req, res, next) {
+  debug('GET /api/login');
+
+  User.findOne({username: req.auth.username})
+  .then(user => user.comparePasswordHash(req.auth.password))
+  .catch(err => Promise.reject(createError(401, err.message)))
+  .then(user => res.status(200).json({user:`${user.id}`}))
+  .catch(next);
+});
+
+authRouter.put('/api/user/:id', bearerAuth, (req, res) => {
+  debug('PUT /api/user/:id');
+
+  User.findByIdAndUpdate({_id: req.params.id }, req.body, {new: true})
+  .then(data => {
+    res.json(data);
+  });
+});
