38e03eecb682427e1aeedbfbed149ab5355e6ef7
Cameron Bacon <cameronmbacon@gmail.com>
2017-06-15 09:50:40 -0700

    completed gallery routes and schema

--- server/route/gallery-router.js
+++ server/route/gallery-router.js
@@ -1,27 +1,79 @@
 'use strict';
 
-const debug = require('debug')('equiGalleryVR:galleryRouter');
-const jsonParser = require('body-parser').json();
+const AWS = require('aws-sdk');
 const Router = require('express').Router;
+const jsonParser = require('body-parser').json();
+const createError = require('http-errors');
+const debug = require('debug')('equiGalleryVR:galleryRouter');
 
+const Photo = require('../model/photo-model');
+const Gallery = require('../model/gallery-model');
+const bearerAuth = require('../lib/bearer-auth-middleware');
+
+const s3 = new AWS.S3();
 const galleryRouter = module.exports = Router();
 
-galleryRouter.post('/api/gallery', jsonParser, function(req, res, next) {
+galleryRouter.post('/api/gallery', bearerAuth, jsonParser, function(req, res, next) {
   debug('POST /api/gallery');
-  res.send(req);
+  req.body.userID = req.user._id;
+  req.body.username = req.user.username;
+  new Gallery(req.body).save()
+  .then(gallery => res.json(gallery))
+  .catch(next);
 });
 
-galleryRouter.put('/api/gallery/:id', jsonParser, function(req, res, next) {
-  debug('PUT /api/gallery');
-  res.send(req);
+galleryRouter.put('/api/gallery/:id', bearerAuth, jsonParser, function(req, res, next) {
+  debug('PUT /api/gallery/:id');
+  Gallery.findById(req.params.id)
+  .catch(err => Promise.reject(createError(404, err.message)))
+  .then(gallery => {
+    if (gallery.userID.toString() !== req.user._id.toString())
+      return Promise.reject(createError(401, 'not this user\'s gallery'));
+    let options = {runValidators: true, new: true};
+    return Gallery.findByIdAndUpdate(req.params.id, req.body, options);
+  })
+  .then(gallery => res.json(gallery))
+  .catch(next);
 });
 
-galleryRouter.get('/api/gallery/:id', function(req, res) { // add 'next' parameter when we implement middleware
+galleryRouter.get('/api/gallery', function(req, res, next) {
   debug('GET /api/gallery');
-  res.send(req);
+  Gallery.find({})
+  .populate('photos')
+  .then(galleries => res.json(galleries))
+  .catch(next);
 });
 
-galleryRouter.delete('/api/gallery/:id', function(req, res) { // add 'next' parameter when we implement middleware
+galleryRouter.get('/api/gallery/:id', function(req, res, next) {
+  debug('GET /api/gallery');
+  Gallery.findById(req.params.id)
+  .populate('photos')
+  .then(gallery => res.json(gallery))
+  .catch(next);
+});
+
+galleryRouter.delete('/api/gallery/:id', function(req, res, next) {
   debug('POST /api/gallery');
-  res.send();
+  let tempGallery = null;
+  Gallery.findById(req.params.id)
+  .populate('photos')
+  .catch(err => Promise.reject(createError(404, err.message)))
+  .then(gallery => {
+    tempGallery = gallery;
+    if (gallery.userID.toString() !== req.user._id)
+      return Promise.reject(createError(404, 'not this user\'s gallery!'));
+    let deletePhotos = [];
+    gallery.photos.forEach(photo => {
+      let params = {
+        Bucket: process.env.AWS_BUCKET,
+        Key: photo.objectKey,
+      };
+      deletePhotos.push(Photo.findByIdAndRemove(photo._id));
+      deletePhotos.push(s3.deleteObject(params).promise());
+    });
+    return Promise.all(deletePhotos);
+  })
+  .then(() => tempGallery.remove())
+  .then(() => res.sendStatus(204))
+  .catch(next);
 });
