4d40f4766253479feeb35b8e06e5d80d194af0dd
Enrique Rico <enriquesantosrico@gmail.com>
2017-06-18 14:12:04 -0700

    tests are running but still need work on passing

--- test/error-middleware-test.js
+++ test/error-middleware-test.js
@@ -25,57 +25,57 @@ function responseMock (){
 
 describe('test errorMiddleware', function(){
   describe('with vanilla Error', function(){
-    let res
+    let res;
     before(() => res = responseMock());
     it('should respond with status 500', done => {
       let err = new Error('regular error');
       errorMiddleware(err, {}, res, () => {
-        expect(res.statusCode.to.equal(500)
-        expect(res.text).to.equal('InternalServerError')
-        done()
-      })
-    })
-  })
+        expect(res.statusCode.to.equal(500));
+        expect(res.text).to.equal('InternalServerError');
+        done();
+      });
+    });
+  });
 
-describe('http-errors error', function(){
-  let res
-  before(() => res = responseMock())
-  it('should respond with status 418', done =>{
-    let err = createError(418, 'example erorr message')
-    errorMiddleware(err, {}, res, () => {
-      expect(res.statusCode).to.equal(418)
-      expect(res.text).to.equal('IamATeapotError')
-      done()
-    })
-  })
-})
+  describe('http-errors error', function(){
+    let res;
+    before(() => res = responseMock());
+    it('should respond with status 418', done =>{
+      let err = createError(418, 'example erorr message');
+      errorMiddleware(err, {}, res, () => {
+        expect(res.statusCode).to.equal(418);
+        expect(res.text).to.equal('IamATeapotError');
+        done();
+      });
+    });
+  });
 
-describe('with ValidationError', function(){
-  let res
-  before(() => res = responseMock())
-  it('should respond with status 418', done => {
-    let err = new Error('You should read the docs')
-    err.name = 'ValidationError'
-    errorMiddleware(err, {}, res, () => {
-      expect(res.statusCode).to.equal(400)
-      expect(res.text).to.equal('BadRequestError')
-      done()
-    })
-  })
-})
+  describe('with ValidationError', function(){
+    let res;
+    before(() => res = responseMock());
+    it('should respond with status 418', done => {
+      let err = new Error('You should read the docs');
+      err.name = 'ValidationError';
+      errorMiddleware(err, {}, res, () => {
+        expect(res.statusCode).to.equal(400);
+        expect(res.text).to.equal('BadRequestError');
+        done();
+      });
+    });
+  });
 
-describe('with MongoError && err.message = "E11000 duplicate"', function(){
-  let res
-  before(() => res = reponseMock())
-  it('should respond with status 409', done => {
-    let err = new Error('E11000 duplicate')
-    err.name = 'MongoError'
-    errorMiddleware(err, {}, res, () => {
-      expect(res.statusCode).to.equal(409)
-      expect(res.text).to.equal('ConflictError')
-      done()
-    })
-  })
-})
+  describe('with MongoError && err.message = "E11000 duplicate"', function(){
+    let res;
+    before(() => res = responseMock());
+    it('should respond with status 409', done => {
+      let err = new Error('E11000 duplicate');
+      err.name = 'MongoError';
+      errorMiddleware(err, {}, res, () => {
+        expect(res.statusCode).to.equal(409);
+        expect(res.text).to.equal('ConflictError');
+        done();
+      });
+    });
+  });
 
 });
