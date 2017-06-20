70938f87b31c0326f22e95c67df6e6d2545130cb
Lindsay Gilbert <Lindsaygilbert722@gmail.com>
2017-06-20 11:34:26 -0700

    add user route

---
+++ public/app/routes.js
@@ -0,0 +1,45 @@
+'use strict';
+
+const app = angular.module('vrControllers', ['ngRoute']);
+
+app.config(function($routeProvider, $locationProvider) {
+
+  $routeProvider
+
+  .when('/', {
+    templateUrl: 'public/app/views/home.html',
+  })
+
+  .when('/signup', {
+    templateUrl: 'public/app/views/pages/users/signup.html',
+    controller: 'signupController',
+    controllerAs: 'signupCtrl',
+  })
+
+  .when('/login', {
+    templateUrl: 'public/app/views/pages/landing/login/login.html',
+  })
+
+  .when('/profile', {
+    templateUrl: 'public/app/views/pages/users/profile.html',
+  })
+
+  .when('/vrfeed', {
+    templateUrl: 'public/app/views/pages/vr/vr-feed.html',
+    controller: 'MainMenuCtrl',
+  })
+
+  .when('/image', {
+    templateUrl: 'public/app/views/pages/vr/vr-image.html',
+    controller: 'ImageCtrl',
+  })
+
+  .otherwise({
+    redirectTo: '/',
+  });
+
+  $locationProvider.html5Mode({
+    enabled: true,
+    requireBase: false,
+  });
+});
