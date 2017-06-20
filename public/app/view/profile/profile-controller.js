92a1a1a6ae061035cc3dab735939a802edcd44df
Brianna <briannaburrows@gmail.com>
2017-06-20 14:29:38 -0700

    removing dead code

--- public/app/view/profile/profile-controller.js
+++ public/app/view/profile/profile-controller.js
@@ -12,20 +12,5 @@ module.exports = [
 function ProfileController($log, $window, $location) {
   this.$onInit = () => {
 
-
-
-    this.galleries = [];
-
-    this.fetchGalleries = () => {
-      return galleryService.fetchGalleries()
-      .then(galleries => {
-        this.galleries = galleries;
-        this.currentGallery = this.galleries[0];
-      })
-      .catch(err => $log.error(err));
-    };
-
-    $rootScope.$on('locationChangeSuccess', this.fetchGalleries);
-    this.fetchGalleries();
   };
 }
